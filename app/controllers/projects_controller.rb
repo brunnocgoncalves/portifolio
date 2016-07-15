f4d8ac529ce066dd9069ef480ca2a416ba2d5c58
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-07-14 23:07:38 -0300

    Fixing User Id on Projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:Name, :Summary, :UserId_id, :Date)
+      params.require(:project).permit(:Name, :Summary, :user_id, :Date)
     end
 end
